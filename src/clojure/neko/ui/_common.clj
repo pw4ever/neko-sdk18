a08853112418ed59688992a0436f49423ab5a540
Alexander Yakushev <yakushev.alex@gmail.com>
2012-06-15 11:42:49 +0300

    Add :edit element

--- src/clojure/neko/ui/_common.clj
+++ src/clojure/neko/ui/_common.clj
@@ -2,15 +2,21 @@
   "Contains the initialization and configuration of the most common
   Android UI elements."
   (:use neko.ui.mapping)
-  (:import [android.widget LinearLayout Button]
+  (:import [android.widget LinearLayout Button EditText]
            [android.view ViewGroup$LayoutParams]))
 
-(defelement :button android.widget.Button
-  :parents [:layout-params :id])
+(defn init-common-elements []
+  (defelement :button android.widget.Button
+    :parents [:layout-params :id])
 
-(defelement :linear-layout android.widget.LinearLayout
-  :parents [:layout-params :id])
+  (defelement :linear-layout android.widget.LinearLayout
+    :parents [:layout-params :id])
 
-(defelement :layout-params ViewGroup$LayoutParams
-  :values {:fill ViewGroup$LayoutParams/FILL_PARENT
-           :wrap ViewGroup$LayoutParams/WRAP_CONTENT})
+  (defelement :edit android.widget.EditText
+    :parents [:id :layout-params])
+
+  (defelement :layout-params ViewGroup$LayoutParams
+    :values {:fill ViewGroup$LayoutParams/FILL_PARENT
+             :wrap ViewGroup$LayoutParams/WRAP_CONTENT}))
+
+(init-common-elements)
