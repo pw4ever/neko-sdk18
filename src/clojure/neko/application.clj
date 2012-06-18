65b9a9ceb0e902fdb0c8c14f18ffa16a8722d0dd
Alexander Yakushev <yakushev.alex@gmail.com>
2012-06-18 20:11:46 +0300

    Add legal notices and remove neko.repl namespace

--- src/clojure/neko/application.clj
+++ src/clojure/neko/application.clj
@@ -1,3 +1,14 @@
+; Copyright © 2012 Alexander Yakushev.
+; All rights reserved.
+;
+; This program and the accompanying materials are made available under the
+; terms of the Eclipse Public License v1.0 which accompanies this distribution,
+; and is available at <http://www.eclipse.org/legal/epl-v10.html>.
+;
+; By using this software in any fashion, you are agreeing to be bound by the
+; terms of this license.  You must not remove this notice, or any other, from
+; this software.
+
 (ns neko.application
   "Contains the tools to create and manipulate Application instances."
   (:use [neko.-utils :only [simple-name]]
--- src/clojure/neko/ui/mapping.clj
+++ src/clojure/neko/ui/mapping.clj
@@ -1,3 +1,14 @@
+; Copyright © 2012 Alexander Yakushev.
+; All rights reserved.
+;
+; This program and the accompanying materials are made available under the
+; terms of the Eclipse Public License v1.0 which accompanies this distribution,
+; and is available at <http://www.eclipse.org/legal/epl-v10.html>.
+;
+; By using this software in any fashion, you are agreeing to be bound by the
+; terms of this license.  You must not remove this notice, or any other, from
+; this software.
+
 (ns neko.ui.mapping
   "This namespace provides utilities to connect the keywords to the
   actual UI classes, define the hierarchy relations between the
