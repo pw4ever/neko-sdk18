31134b6b6b78ecebb15385ee645a1e7dced19dd8
Alexander Yakushev <yakushev.alex@gmail.com>
2012-08-05 14:30:41 +0300

    Prepare 2.0.0-alpha2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject neko "2.0.0-SNAPSHOT"
+(defproject neko "2.0.0-alpha2"
   :description "Neko is a toolkit designed to make Android development using Clojure easier and more fun."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
