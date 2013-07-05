5e447d46e51a16ed99071bb0590d6726219e70e5
Alexander Yakushev <alex@bytopia.org>
2013-07-05 19:43:22 +0300

    Cosmetic redesign of deftrait macro
    Drop unnecessary fn and wrapping into a list

--- src/clojure/neko/ui/traits.clj
+++ src/clojure/neko/ui/traits.clj
@@ -16,7 +16,7 @@
             [neko.context :as context]
             [neko.listeners.view :as view-listeners])
   (:use [neko.-utils :only [memoized]])
-  (:import [android.widget LinearLayout$LayoutParams]
+  (:import [android.widget LinearLayout$LayoutParams TextView]
            [android.view View ViewGroup$LayoutParams]
            android.util.TypedValue
            java.util.HashMap))
@@ -67,9 +67,10 @@ next-level elements."
   (let [[docstring args] (if (string? (first args))
                            [(first args) (next args)]
                            [nil args])
-        match-pred (if (> (count args) 1)
-                     (first args) name)
-        codegen-body (last args)
+        [match-pred args] (if-not (vector? (first args))
+                            [(first args) (next args)]
+                            [name args])
+        codegen-body args
         dissoc-fn `(fn [a#] (dissoc a# ~name))]
     `(do
        (alter-meta! #'apply-trait
@@ -77,7 +78,7 @@ next-level elements."
        (defmethod apply-trait ~name
          [trait# widget# attributes# options#]
          (if (~match-pred attributes#)
-           (let [result# (~codegen-body widget# attributes# options#)]
+           (let [result# ((fn ~@codegen-body) widget# attributes# options#)]
              (if (map? result#)
                [(:attributes-fn result# ~dissoc-fn)
                 (:options-fn result# identity)]
@@ -93,19 +94,18 @@ next-level elements."
 
   Example: `[:button {:def ok}]` defines a var `ok` which stores the
   button object."
-  (fn [wdg attributes _]
-    (let [sym (:def attributes)]
-      (intern (symbol (namespace sym)) (symbol (name sym)) wdg))))
+  [wdg {:keys [def]} _]
+  (intern (symbol (namespace def)) (symbol (name def)) wdg))
 
 ;; ### Basic traits
 
 (deftrait :text
   "Sets widget's text to a string, integer ID or a keyword
   representing the string resource provided to `:text` attribute."
-  (fn [wdg {:keys [text]} _]
-    (.setText ^android.widget.TextView wdg (if (keyword? text)
-                                             ^int (res/get-string text)
-                                             ^String text))))
+  [^TextView wdg, {:keys [text]} _]
+  (if (keyword? text)
+    (.setText wdg ^int (res/get-string text))
+    (.setText wdg ^CharSequence text)))
 
 ;; ### Layout parameters attributes
 
@@ -130,17 +130,17 @@ next-level elements."
   attributes and sets a proper LayoutParams according to container
   type."
   #(some % [:layout-width :layout-height :layout-weight])
-  (fn [wdg {:keys [layout-width layout-height layout-weight]}
-      {:keys [container-type]}]
-    (case (kw/keyword-by-classname container-type)
-      :linear-layout
-      (.setLayoutParams ^View
-       wdg (linear-layout-params layout-width layout-height layout-weight))
+  [^View wdg, {:keys [layout-width layout-height layout-weight]}
+   {:keys [container-type]}]
+  (case (kw/keyword-by-classname container-type)
+    :linear-layout
+    (.setLayoutParams
+     wdg (linear-layout-params layout-width layout-height layout-weight))
 
-      (.setLayoutParams ^View
-       wdg (default-layout-params layout-width layout-height)))
-    {:attributes-fn #(dissoc % :layout-width :layout-height
-                             :layout-weight)}))
+    (.setLayoutParams
+     wdg (default-layout-params layout-width layout-height)))
+  {:attributes-fn #(dissoc % :layout-width :layout-height
+                           :layout-weight)})
 
 (defn- kw->unit-id [unit-kw]
   (case unit-kw
@@ -173,66 +173,64 @@ next-level elements."
   vectors like `[number unit-kw]`, where unit keyword is one of the
   following: :px, :dip, :sp, :pt, :in, :mm."
   #(some % [:padding :padding-bottom :padding-left :padding-right :padding-top])
-  (fn [wdg {:keys [padding padding-bottom padding-left
-                  padding-right padding-top]} _]
-    (.setPadding ^View wdg
-                 (padding-value->pixels (or padding-left padding 0))
-                 (padding-value->pixels (or padding-top padding 0))
-                 (padding-value->pixels (or padding-right padding 0))
-                 (padding-value->pixels (or padding-bottom padding 0)))
-    {:attributes-fn #(dissoc % :padding :padding-bottom :padding-left
-                             :padding-right :padding-top)}))
+  [wdg {:keys [padding padding-bottom padding-left
+               padding-right padding-top]} _]
+  (.setPadding ^View wdg
+               (padding-value->pixels (or padding-left padding 0))
+               (padding-value->pixels (or padding-top padding 0))
+               (padding-value->pixels (or padding-right padding 0))
+               (padding-value->pixels (or padding-bottom padding 0)))
+  {:attributes-fn #(dissoc % :padding :padding-bottom :padding-left
+                           :padding-right :padding-top)})
 
 (deftrait :container
-  "Puts the type of the widget its type into the options map so
-  subelement can use the container type to choose the correct
-  LayoutParams instance."
+  "Puts the type of the widget onto the options map so subelement can
+  use the container type to choose the correct LayoutParams instance."
   (constantly true)
-  (fn [wdg _ __]
-    {:options-fn #(assoc % :container-type (type wdg))}))
+  [wdg _ __]
+  {:options-fn #(assoc % :container-type (type wdg))})
 
 ;; ### Listener traits
 
 (deftrait :on-click
   "Takes :on-click attribute, which should be function of one
   argument, and sets it as an OnClickListener for the widget."
-  (fn [wdg {:keys [on-click]} _]
-    (.setOnClickListener ^View wdg (view-listeners/on-click-call on-click))))
+  [^View wdg, {:keys [on-click]} _]
+  (.setOnClickListener wdg (view-listeners/on-click-call on-click)))
 
 (deftrait :on-create-context-menu
   "Takes :on-create-context-menu attribute, which should be function
   of three arguments, and sets it as an OnCreateContextMenuListener
   for the object."
-  (fn [wdg {:keys [on-create-context-menu]} _]
-    (.setOnCreateContextMenuListener ^View
-     wdg (view-listeners/on-create-context-menu-call on-create-context-menu))))
+  [^View wdg, {:keys [on-create-context-menu]} _]
+  (.setOnCreateContextMenuListener
+   wdg (view-listeners/on-create-context-menu-call on-create-context-menu)))
 
 (deftrait :on-focus-change
   "Takes :on-focus-change attribute, which should be function of two
   arguments, and sets it as an OnFocusChangeListener for the object."
-  (fn [wdg {:keys [on-focus-change]} _]
-    (.setOnFocusChangeListener ^View
-     wdg (view-listeners/on-focus-change-call on-focus-change))))
+  [^View wdg, {:keys [on-focus-change]} _]
+  (.setOnFocusChangeListener
+   wdg (view-listeners/on-focus-change-call on-focus-change)))
 
 (deftrait :on-key
   "Takes :on-key attribute, which should be function of three
   arguments, and sets it as an OnKeyListener for the widget."
-  (fn [wdg {:keys [on-key]} _]
-    (.setOnKeyListener ^View wdg (view-listeners/on-key-call on-key))))
+  [^View wdg, {:keys [on-key]} _]
+  (.setOnKeyListener wdg (view-listeners/on-key-call on-key)))
 
 (deftrait :on-long-click
   "Takes :on-long-click attribute, which should be function of one
   argument, and sets it as an OnLongClickListener for the widget."
-  (fn [wdg {:keys [on-long-click]} code _]
-    (.setOnLongClickListener ^View
-     wdg (view-listeners/on-long-click-call on-long-click))))
+  [^View wdg, {:keys [on-long-click]} _]
+  (.setOnLongClickListener
+   wdg (view-listeners/on-long-click-call on-long-click)))
 
 (deftrait :on-touch
   "Takes :on-touch attribute, which should be function of two
   arguments, and sets it as an OnTouchListener for the widget."
-  (fn [wdg {:keys [on-touch]} code _]
-    (.setOnTouchListener ^View
-     wdg (view-listeners/on-touch-call on-touch))))
+  [^View wdg, {:keys [on-touch]} _]
+  (.setOnTouchListener wdg (view-listeners/on-touch-call on-touch)))
 
 ;; ### ID storing traits
 
@@ -246,15 +244,15 @@ next-level elements."
   (def foo (make-ui [:linear-layout {:id-holder true}
                      [:button {:id ::abutton}]]))
   (::abutton (.getTag foo)) => internal Button wdgect."
-  (fn [wdg _ __]
-    (.setTag ^View wdg (HashMap.))
-    {:options-fn #(assoc % :id-holder wdg)}))
+  [^View wdg, _ __]
+  (.setTag wdg (HashMap.))
+  {:options-fn #(assoc % :id-holder wdg)})
 
 (deftrait :id
   "Takes `:id` attribute, which should be a keyword, and stores the
   current widget in ID-holder's tag (see docs for `:id-holder`
   trait)."
-  (fn [wdg {:keys [id]} {:keys [^View id-holder]}]
-    (if (nil? id-holder)
-      (throw (Exception. ":id trait: id-holder is undefined in this UI tree."))
-      (.put ^HashMap (.getTag id-holder) id wdg))))
+  [wdg {:keys [id]} {:keys [^View id-holder]}]
+  (if (nil? id-holder)
+    (throw (Exception. ":id trait: id-holder is undefined in this UI tree."))
+    (.put ^HashMap (.getTag id-holder) id wdg)))
