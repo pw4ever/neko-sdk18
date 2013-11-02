b96b5f8f45f1d57c8daf8fb5c78225e2a4c1efe9
Adam Clements <adam.clements@gmail.com>
2013-11-02 10:57:45 +0100

    Completely rewrite logging namespace

--- src/clojure/neko/log.clj
+++ src/clojure/neko/log.clj
@@ -10,64 +10,56 @@
 ; this software.
 
 (ns neko.log
-  "Utility for logging in Android.  To use this utility simply use the deflog
-  macro as follows:
+  "Utility for logging in Android. There are five logging macros: `i`,
+  `d`, `e`, `v`, `w`; for different purposes. Each of them takes
+  variable number of arguments and optional keyword arguments at the
+  end: `:exception` and `:tag`. If `:tag` is not provided, current
+  namespace is used instead. Examples:
 
-    (deflog \"MyTag\")
+    (require '[neko.log :as log])
 
-  This will intern a number of functions in your namespace, namely: log-d,
-  log-v, log-i, log-w, log-e, and log-wtf.  These functions will perform the
-  equivalent of calling the corresponding method on the android.util.Log
-  class with the tag given in the deflog macro call.
-
-  For example, given the above deflog call,
-
-    (log-d \"Some log string\")
-
-  is equivalent to:
-
-    (android.util.Log/d \"MyTag\" \"Some log string\")
-
-  Calls to log-wtf on platforms that do not support Log.wtf() will be
-  downgraded to Log.e() calls."
-  {:author "Daniel Solano Gómez"}
-  (:require neko.context)
+    (neko.log/d \"Some log string\" {:foo 1, :bar 2})
+    (neko.log/i \"Logging to custom tag\" [1 2 3] :tag \"custom\")
+    (neko.log/e \"Something went wrong\" [1 2 3] :exception ex)"
+  {:author "Adam Clements"}
   (:import android.util.Log))
 
-(defmacro deflogfn
-  "Macro for generating log functions."
-  {:private true}
-  [fn-name doc-string method-name]
-  `(defn ~fn-name
-     ~doc-string
-     ([^String tag#, ^String message#]
-      (. Log (~method-name tag# message#)))
-     ([^String tag#, ^String, message#, ^Throwable throwable#]
-      (. Log (~method-name tag# message# throwable#)))))
+(defn- logger [logfn args]
+  (let [[strings kwargs] (split-with (complement #{:exception :tag}) args)
+        {:keys [exception tag]} (if (odd? (count kwargs))
+                                  (butlast kwargs)
+                                  kwargs)
+        my-ns (str *ns*)]
+    (if exception
+      `(. Log ~logfn ~(or tag my-ns) (pr-str ~@strings) ~exception)
+      `(. Log ~logfn ~(or tag my-ns) (pr-str ~@strings)))))
 
-(deflogfn d "Sends a DEBUG log message." d)
-(deflogfn e "Sends a ERROR log message." e)
-(deflogfn i "Sends a INFO log message." i)
-(deflogfn v "Sends a VERBOSE log message." v)
-(deflogfn w "Sends a WARN log message." w)
+(defmacro e
+  "Log an ERROR message, applying pr-str to all the arguments and taking
+   an optional keyword :exception or :tag at the end which will print the
+   exception stacktrace or override the TAG respectively"
+  [& args] (logger 'e args))
 
-(defn log-exception
-  "Takes a Throwable instance and logs its stacktrace with error priority."
-  [throwable]
-  (e (.getPackageName neko.context/context)
-     (android.util.Log/getStackTraceString throwable)))
+(defmacro d
+  "Log a DEBUG message, applying pr-str to all the arguments and taking
+   an optional keyword :exception or :tag at the end which will print the
+   exception stacktrace or override the TAG respectively"
+  [& args] (logger 'd args))
 
-(defmacro deflog
-  "Creates a number of logging functions for the given tag."
-  [tag]
-  (let [intern-logger
-        (fn [log-name log-fn]
-          `(intern *ns* (symbol ~log-name)
-                   (-> (partial ~log-fn ~tag)
-                       (with-meta  {:private true}))))]
-    `(do
-       ~(intern-logger "log-d" `d)
-       ~(intern-logger "log-e" `e)
-       ~(intern-logger "log-i" `i)
-       ~(intern-logger "log-v" `v)
-       ~(intern-logger "log-w" `w))))
+(defmacro i
+  "Log an INFO message, applying pr-str to all the arguments and taking
+   an optional keyword :exception or :tag at the end which will print the
+   exception stacktrace or override the TAG respectively"
+  [& args] (logger 'i args))
+
+(defmacro v
+  "Log a VERBOSE message, applying pr-str to all the arguments and taking
+   an optional keyword :exception or :tag at the end which will print the
+   exception stacktrace or override the TAG respectively"
+  [& args] (logger 'v args))
+
+(defmacro w
+  "Log a WARN message, applying pr-str to all the arguments and taking
+   an optional keyword :exception or :tag at the end which will print the
+   exception stacktrace or override the TAG respectively"
+  [& args] (logger 'w args))
