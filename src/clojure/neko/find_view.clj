8f96871dc714ed0517c2390755d5fd45844b2e82
Alexander Yakushev <alex@bytopia.org>
2014-04-01 21:26:45 +0200

    Rewrite `find-view` to support neko.ui's :id trait

--- src/clojure/neko/find_view.clj
+++ src/clojure/neko/find_view.clj
@@ -1,32 +1,10 @@
-; Copyright © 2011 Sattvik Software & Technology Resources, Ltd. Co.
-; All rights reserved.
-;
-; This program and the accompanying materials are made available under the
-; terms of the Eclipse Public License v1.0 which accompanies this distribution,
-; and is available at <http://www.eclipse.org/legal/epl-v10.html>.
-;
-; By using this software in any fashion, you are agreeing to be bound by the
-; terms of this license.  You must not remove this notice, or any other, from
-; this software.
-
 (ns neko.find-view
-  "Home of the ViewFinder protocol, which should simplify and unify use of the
-  findViewById method introduced in various Android classes.  To use the
-  protocol, you need an object that supports findViewById, and these are:
-
-  + activities
-  + dialogs
-  + views
-  + windows
-
-  Given one of these objects, you can (find-view obj id).
-
-  In addition, if within a with-activity form, you can leave out the activity
-  argument, simplifying the above call to:
-
-    (find-view R$id/my_view)"
-  {:author "Daniel Solano Gómez"}
-  (:use neko.activity))
+  "Home of the ViewFinder protocol, which should simplify and unify
+  obtaining UI elements by their :id trait. To use the protocol an
+  object has to keep a mapping of IDs to UI widgets. Currently the
+  protocol is supported by Activity and View objects."
+  (:import android.view.View
+           android.app.Activity))
 
 (defn- nil-or-view?
   [x]
@@ -34,45 +12,22 @@
       (instance? android.view.View x)))
 
 (defprotocol ViewFinder
-  "Protocol for finding child views by an ID."
-  (find-view [id] [finder id]
-    "The two-arg version is the general version used outside of any context.
-    The one-arg version is designed for use within a (with-activity)
-    context."))
+  "Protocol for finding child views by their `:id` trait."
+  (find-view [container id]))
 
 (extend-protocol ViewFinder
-  android.view.Window
-  (find-view [window id]
-    {:pre  [(integer? id)]
-     :post [(nil-or-view? %)]}
-    (.findViewById window id))
+  View
+  (find-view [^View view, id]
+    {:post [(nil-or-view? %)]}
+    (get (.getTag view) id))
 
-  android.app.Activity
-  (find-view [activity id]
-    {:pre  [(integer? id)]
-     :post [(nil-or-view? %)]}
-    (.findViewById activity id))
+  Activity
+  (find-view [^Activity activity, id]
+    {:post [(nil-or-view? %)]}
+    (get (.. activity getWindow getDecorView getTag) id)))
 
-  android.view.View
-  (find-view [view id]
-    {:pre  [(integer? id)]
-     :post [(nil-or-view? %)]}
-    (.findViewById view id))
-
-  android.app.Dialog
-  (find-view [dialog id]
-    {:pre  [(integer? id)]
-     :post [(nil-or-view? %)]}
-    (.findViewById dialog id))
-
-  Integer
-  (find-view [id]
-    {:pre [(has-*activity*?)]
-     :post [(nil-or-view? %)]}
-    (find-view *activity* id))
-
-  Long
-  (find-view [id]
-    {:pre [(has-*activity*?)]
-     :post [(nil-or-view? %)]}
-    (find-view *activity* (.intValue id))))
+(defn find-views
+  "Same as `find-view`, but takes a variable number of IDs and returns
+a vector of found views."
+  [container & ids]
+  (map (partial find-view container) ids))
