fa0fe8246566fbf4ad2cef5d92c6cd79353386f3
Daniel Solano Gómez <daniel@solanogomez.org>
2014-05-10 20:15:37 -0500

    Update GitHub path to clojure-android

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject neko "3.0.2-SNAPSHOT"
   :description "Neko is a toolkit designed to make Android development using Clojure easier and more fun."
-  :url "https://github.com/alexander-yakushev/neko"
+  :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure-android/clojure "1.5.1-jb"]]
