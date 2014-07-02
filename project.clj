29ca5fd4c7f3e799aa357f16b6a9f8925a10c734
Wei Peng <write.to.peng.wei@gmail.com>
2014-07-02 18:07:14 -0400

    update project.clj to build for sdk18 and upload to clojars

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject neko "3.0.2"
+(defproject org.clojars.pw4ever/neko-sdk18 "3.0.2"
   :description "Neko is a toolkit designed to make Android development using Clojure easier and more fun."
   :url "https://github.com/clojure-android/neko"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure-android/clojure "1.5.1-jb"]]
+  :dependencies [[org.clojure-android/clojure "1.6.0-RC1"]]
   :source-paths ["src" "src/clojure"]
   :java-source-paths ["src/java" "gen"]
 
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :android {:library true
-            :target-version :ics})
+            :target-version 18})
